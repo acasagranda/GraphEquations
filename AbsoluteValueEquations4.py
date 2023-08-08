27019642e9e1415f07888d53a91cd7c5abd488d1
acasagranda <ybblkblt@yahoo.com>
2023-08-08 11:51:26 -0400

    positive coefficient on abs val

---
+++ AbsoluteValueEquations4.py
@@ -0,0 +1,397 @@
+import numpy as np                 
+import matplotlib.pyplot as plt    
+import random
+
+
+class intrograph:
+    
+    def __init__(self):
+
+        #make two columns of graphs
+        self.fig, (self.ax1,self.ax2) = plt.subplots(1,2,figsize=(12,12))
+
+        #set up size of graphs
+        self.axisdim = 5
+        
+        #set up axes
+        self.ax1.set(xlim=(-self.axisdim, self.axisdim), ylim=(-self.axisdim, self.axisdim), aspect='equal')
+        self.ax2.set(xlim=(-self.axisdim, self.axisdim), ylim=(-self.axisdim, self.axisdim), aspect='equal')
+        self.ax1.spines['bottom'].set_position('zero')
+        self.ax1.spines['left'].set_position('zero')
+        self.ax1.spines['top'].set_visible(False)
+        self.ax1.spines['right'].set_visible(False)
+        self.ax2.spines['bottom'].set_position('zero')
+        self.ax2.spines['left'].set_position('zero')
+        self.ax2.spines['top'].set_visible(False)
+        self.ax2.spines['right'].set_visible(False)
+
+        # Create 'x' and 'y' labels placed at the end of the axes
+        self.ax1.set_xlabel('x', size=14, labelpad=-24, x=1.03) 
+        self.ax1.set_ylabel('y', size=14, labelpad=-21, y=1.02, rotation=0)
+        self.ax2.set_xlabel('x', size=14, labelpad=-24, x=1.03)
+        self.ax2.set_ylabel('y', size=14, labelpad=-21, y=1.02, rotation=0)
+
+        #set up ticks
+        self.ax1.set_xticks([-5,-4,-3,-2,-1,1,2,3,4,5])
+        self.ax1.set_yticks([-5,-4,-3,-2,-1,1,2,3,4,5])
+        self.ax2.set_xticks([-5,-4,-3,-2,-1,1,2,3,4,5])
+        self.ax2.set_yticks([-5,-4,-3,-2,-1,1,2,3,4,5])
+
+        #put grid behind graphs
+        self.ax2.grid(True)
+        self.ax1.grid(True)
+
+        #set up arrows
+        arrow_fmt = dict(markersize=4, color='black', clip_on=False)
+        self.ax1.plot((1), (0), marker='>', transform=self.ax1.get_yaxis_transform(), **arrow_fmt)
+        self.ax1.plot((0), (1), marker='^', transform=self.ax1.get_xaxis_transform(), **arrow_fmt)
+        self.ax2.plot((1), (0), marker='>', transform=self.ax2.get_yaxis_transform(), **arrow_fmt)
+        self.ax2.plot((0), (1), marker='^', transform=self.ax2.get_xaxis_transform(), **arrow_fmt)
+
+        
+    def show(self):
+
+        # Enter x and y coordinates of points and colors
+        x1 = [-3, 1]
+        y1 = [3, 1]
+        self.ax1.scatter(x1, y1,c='k')
+        # self.ax2.scatter(1, 4,c='b')
+        # self.ax2.scatter(1, 0,c='m')
+        # self.ax2.scatter(1, 1,c='k')
+
+        #set up lines putting label here used in legend
+        x1 =np.linspace(-self.axisdim,0)
+        x2=np.linspace(0,self.axisdim)
+        y1 = abs(x1)
+        y2 = abs(x2)
+        self.ax1.plot(x1, y1,c='k',lw=1.5, label = pe)
+        self.ax1.plot(x2, y2,c='k',lw=1.5)
+        self.ax2.plot(x1, 3*y1,c='b',lw=1.5, label="y = 3|x|")
+        self.ax2.plot(x1, y1,c='k',lw=1.5, label=pe)
+        self.ax2.plot(x1, y1/4,c='m',lw=1.5, label="y = 1/4 |x|")
+        self.ax2.plot(x2, y2*3,c='b',lw=1.5)
+        self.ax2.plot(x2, y2,c='k',lw=1.5,)
+        self.ax2.plot(x2, y2/4,c='m',lw=1.5)
+
+        #set up legend
+        self.ax1.legend(loc="lower right")
+        self.ax2.legend(loc="lower right")
+
+        #set up text
+        self.ax1.text(-5,6,text1)
+        self.ax2.text(-5,6,text2)
+        self.ax2.text(-5,-6.5,text3)
+
+        #display
+        plt.show()
+
+
+class taskgraph:
+    
+    def __init__(self,m,frac,scale,text1,text2):
+        self.m=m
+        self.frac=frac
+        self.scale=scale
+        self.text1=text1
+        self.text2=text2
+
+        #set up size of graph
+        self.axisdim=self.scale*10+10
+
+        #make to columns of graphs
+        self.fig, (self.ax,self.textarea) = plt.subplots(1,2,figsize=(13,13))
+
+        #set up axes
+        self.ax.set(xlim=(-self.axisdim, self.axisdim), ylim=(-self.axisdim, self.axisdim), aspect='equal')
+        self.ax.spines['bottom'].set_position('zero')
+        self.ax.spines['left'].set_position('zero')
+        self.ax.spines['top'].set_visible(False)
+        self.ax.spines['right'].set_visible(False)
+        self.textarea.axis('off')
+
+        # Create 'x' and 'y' labels placed at the end of the axes
+        self.ax.set_xlabel('x', size=14, labelpad=-24, x=1.03) 
+        self.ax.set_ylabel('y', size=14, labelpad=-21, y=1.02, rotation=0)
+        
+        # Hide X and Y axes label marks
+        self.ax.xaxis.set_tick_params(labelbottom=False)
+        self.ax.yaxis.set_tick_params(labelleft=False)
+
+        # Hide X and Y axes tick marks
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
+       
+        #no background grid
+        self.ax.grid(False)
+        # set up arrows
+
+        arrow_fmt = dict(markersize=4, color='black', clip_on=False)
+        self.ax.plot((1), (0), marker='>', transform=self.ax.get_yaxis_transform(), **arrow_fmt)
+        self.ax.plot((0), (1), marker='^', transform=self.ax.get_xaxis_transform(), **arrow_fmt)
+
+
+        
+    def show(self):
+        #set up lines putting label here used in legend
+        x1 =np.linspace(-self.axisdim,0)
+        x2=np.linspace(0,self.axisdim)
+        y1 = abs(x1)
+        y2 = abs(x2)
+        if self.frac:
+            y3 = abs(x1) / self.m
+            y4 = abs(x2) / self.m
+        else:
+            y3 = abs(x1) * self.m
+            y4 = abs(x2) * self.m
+        self.ax.plot(x1, y1,c='k',lw=1.5,label = pe)
+        self.ax.plot(x1, y3,c='r',lw=1.5,label = "y = ?")
+        self.ax.plot(x2, y2,c='k',lw=1.5)
+        self.ax.plot(x2, y4,c='r',lw=1.5)
+
+        #set up legend
+        self.ax.legend(loc="lower right")
+        
+        #set up text
+        self.textarea.text(0,.3,self.text1 + self.text2,fontsize=12)
+        plt.show()
+
+
+class guessgraph:
+    
+    def __init__(self,m,frac,targeteq,scale):
+        self.targeteq = targeteq
+        self.m=m
+        self.frac=frac
+        self.scale=scale
+
+        #set up size of graph
+        self.axisdim=self.scale*10+10
+
+        #make to columns of graphs
+        self.fig, (self.ax,self.textarea) = plt.subplots(1,2,figsize=(13,13))
+
+        #set up axes
+        self.ax.set(xlim=(-self.axisdim, self.axisdim), ylim=(-self.axisdim, self.axisdim), aspect='equal')
+        self.ax.spines['bottom'].set_position('zero')
+        self.ax.spines['left'].set_position('zero')
+        self.ax.spines['top'].set_visible(False)
+        self.ax.spines['right'].set_visible(False)
+        self.textarea.axis('off')
+
+        # Create 'x' and 'y' labels placed at the end of the axes
+        self.ax.set_xlabel('x', size=14, labelpad=-24, x=1.03) 
+        self.ax.set_ylabel('y', size=14, labelpad=-21, y=1.02, rotation=0)
+        
+        # Hide X and Y axes label marks
+        self.ax.xaxis.set_tick_params(labelbottom=False)
+        self.ax.yaxis.set_tick_params(labelleft=False)
+
+        # Hide X and Y axes tick marks
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
+       
+        #no background grid
+        self.ax.grid(False)
+
+        # set up arrows
+        arrow_fmt = dict(markersize=4, color='black', clip_on=False)
+        self.ax.plot((1), (0), marker='>', transform=self.ax.get_yaxis_transform(), **arrow_fmt)
+        self.ax.plot((0), (1), marker='^', transform=self.ax.get_xaxis_transform(), **arrow_fmt)
+        
+        
+        
+    def show(self,text1,text2,answer):
+        self.text1=text1
+        self.text2=text2
+        self.answer=answer
+        
+        #set up lines putting label here used in legend
+        x1 =np.linspace(-self.axisdim,0)
+        x2=np.linspace(0,self.axisdim)
+        y1 = abs(x1)
+        y2 = abs(x2)
+        if self.frac:
+            y3 = abs(x1) / self.m
+            y4 = abs(x2) / self.m
+        else:
+            y3 = abs(x1) * self.m
+            y4 = abs(x2) * self.m
+        self.ax.plot(x1, y1,c='k',lw=1.5,label = pe)
+        self.ax.plot(x2, y2,c='k',lw=1.5)
+        
+        if self.answer:  
+            self.ax.plot(x1, y3,c='r',lw=3.5,label = self.targeteq)
+            self.ax.plot(x2, y4,c='r',lw=3.5)
+        else:
+            self.ax.plot(x1, y3,c='r',lw=1.5,label = "y = ?")
+            self.ax.plot(x2, y4,c='r',lw=1.5)
+        
+        for idx in range(len(guessm)):
+            if guessf[idx]:
+                self.ax.plot(x1, abs(x1)/guessm[idx],c=colors[idx],lw=1.5,label = guesseq[idx])
+                self.ax.plot(x2, abs(x2)/guessm[idx],c=colors[idx],lw=1.5)
+            else:
+                self.ax.plot(x1, abs(x1)*guessm[idx],c=colors[idx],lw=1.5,label = guesseq[idx])
+                self.ax.plot(x2, abs(x2)*guessm[idx],c=colors[idx],lw=1.5)
+
+        #set up legend
+        self.ax.legend(loc="lower right")
+        
+        #set up text
+        self.textarea.text(0,.6,self.text1,fontsize=12)
+        self.textarea.text(0,.4,self.text2,fontsize=12)
+        plt.show()
+
+
+
+def geteq():
+    errortext=f"Equations should follow the format\
+        \n      y = a|x|    or    y = 1/a |x| \nwhere a is an integer between 2 and 8.\
+        \n\n (The | key is typically on the keyboard between the Enter and Backspace keys.)\n"
+    while True:
+        userin=input("The equation of the red graph could be:")
+        print("\n")
+        userin2="".join(userin.split())
+        userin2=userin2.lower()
+        if len(userin2)<6 or userin2[:2]!= "y=":
+            print(errortext)
+            continue
+        
+        m=userin2[2]
+        if not m.isnumeric():
+            print(errortext)
+            continue
+        frac=False
+        if m=='1':
+            if userin2[3]!='/':
+                print(errortext)
+                continue
+            frac=True
+            m=userin2[4]
+            if userin2[5:]!='|x|':
+                print(errortext)
+                continue
+        else:
+            if userin2[3:]!='|x|':
+                print(errortext)
+                continue
+        m=int(m)
+        if not (2<=m<=8):
+            print(errortext)
+            continue
+
+        return m,frac,userin
+
+def getagain():
+    ans=input("Do you want to start a new challenge? Type y or n:")
+    ans=ans.lower()
+    if ans=='y':
+        return True
+    elif ans=='n':
+        return False
+    else:
+         return getagain()
+    
+    
+
+
+#set up colors of guesses
+colors=['darkviolet','blue','deepskyblue','green','chartreuse','yellow','orange']
+
+#set up parent equation and texts for intrograph
+pe="y = |x|"
+text1="This is a graph of "+ pe + ".\n\nAll of the points on the right side of the V\nhave the same x-coordinate and y-coordinate;\
+                 \nfor example, (1,1).  This is part of the line y = x.\
+                  \n\nAll of the points on the left hand side\nhave a y-coordinate that is the positive of the x-coordinate;\
+                   \nfor example, (-3,3).  This is part of the line y = -x."
+text2="If we tilt the sides of the graph,\nthe x- and y-coordinates of the points are no longer the same.\
+                 \n\nThis means that the equation of the new line cannot be "+ pe +"."
+text3="The equation changes by " + r"$\bf{"+ 'multiplying' +"}$" + " |x| by a number.\
+                 \n\nClose this graph window when you are ready to move on."
+
+
+g1=intrograph()
+g1.show()
+
+
+#choose c of first and second target lines
+list1 = [5,6,7,8,9]
+m1=random.choice(list1)
+list1.remove(m1)
+list1.append(3)
+m2=random.choice(list1)
+mlist=[m2,m1]
+flist=[True,False]
+targeteq1="y = "+str(m1)+"|x|"
+targeteq2="y = 1/"+str(m2)+" |x|"
+targeteqlist=[targeteq2,targeteq1]
+
+#set up texts for taskgraph
+text1="The "+ r"$\bf{"+ 'black' +"}$" + f" graph to the left is the \'parent\' function " + pe +"."
+text2="\n\nYour task is to find the equation of the "+ r"$\bf{"+ 'red' +"}$" + f" graph.\
+                      \n\nSince there are no marks on the graph, you'll have to guess\
+                      \nand then use the result to get closer and closer\
+                      \nuntil you find the red graph\'s equaton.\
+                      \n\nThe equations in this puzzle will either look like\
+                      \ny = a|x| or y = 1/a |x|\
+                      \nwhere a is an integer between 2 and 8.\
+                      \n\n(The equation can actually by y = c/a |x| for any c but\
+                      \nwe are limiting it to 1/a for this puzzle.)\
+                      \n\nWhen you have your guess,\
+                      \nclose the graph window and enter it into the terminal."
+again=True
+while again:
+    #choose random scale of axes
+    scale=random.random()
+    if mlist:
+        m=mlist.pop()
+        frac=flist.pop()
+        targeteq=targeteqlist.pop()
+    else:
+        m=random.randrange(2, 8)
+        frac=random.choice([True,False])
+        if frac:
+            targeteq="y = 1/"+str(m)+" |x|"
+        else:
+            targeteq="y = "+str(m)+"|x|"
+
+    #set up taskgraph
+    g2=taskgraph(m,frac,scale,text1,text2)
+    g2.show()
+
+    #set up texts for guessgraph
+    text3="Use the results to make another guess."
+    text4="Close the graph window and enter your next guess in the terminal."
+
+
+    #user can take up to 8 guesses - last one answer is revealed
+    guessm=[]
+    guessf=[]
+    guesseq=[]
+    for turn in range(8):
+        userm,userf,usereq=geteq()
+
+        if userm==m and userf==frac:
+            found=True
+            text5="That is correct!! \n\nThe red graph has the equation "+usereq
+            text6="Close the graph window when you are ready to move on."
+            g3=guessgraph(m,frac,usereq,scale)
+            g3.show(text5,text6,True)
+            break
+        elif turn!=7:
+            guessm.append(userm)
+            guessf.append(userf)
+            guesseq.append(usereq)
+            g3=guessgraph(m,frac,targeteq,scale)
+            g3.show(text3,text4,False)
+        else:
+            text7="The red graph has the equation "+targeteq
+            text8="Close the graph window when you are ready to move on."
+            g3=guessgraph(m,frac,targeteq,scale)
+            g3.show(text7,text8,True)
+    again=getagain()
+
+
+
+
+
